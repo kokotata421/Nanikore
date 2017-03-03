b04de5d22334914c091930b658afefcd03a67076
川西弘泰 <kokotata421@i.softbank.jp>
2017-03-04 08:28:25 +0900

    Initial Commit

---
+++ Nanikore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nanikore
+//
+//  Created by Kota Kawanishi on 2017/03/04.
+//  Copyright © 2017年 Kota Kawanishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
